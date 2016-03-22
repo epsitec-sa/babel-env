399cabaab70a049d00f86a5e7fd73e00681dc3d2
Mathieu Schroeter <mathieu@schroetersa.ch>
2016-03-22 18:12:25 +0100

    Use the right line terminator accordingly to the platform

--- bin/babel-env.js
+++ bin/babel-env.js
@@ -11,5 +11,13 @@ var suffix = '/node_modules/babel-env';
 if (cwd.endsWith (suffix)) {
   var root = cwd.substr (0, cwd.length - suffix.length);
   var babelrc = fs.readFileSync (path.join (cwd, 'templates', '.babelrc'));
-  fs.writeFileSync (path.join (root, '.babelrc'), babelrc);
+
+  var regex = /\r\n/g;
+  var newline = '\n';
+  if (process.platform === 'win32') {
+    regex = /[^\r]\n/g;
+    newline = '\r\n';
+  }
+
+  fs.writeFileSync (path.join (root, '.babelrc'), babelrc.replace (regex, newline));
 }
